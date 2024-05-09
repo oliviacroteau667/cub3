72004e0f07b6e3f3b9866623adead42dbed72d96
oliviacroteau667 <111761829+oliviacroteau667@users.noreply.github.com>
2024-05-08 20:41:10 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+/**
+ * Olivia Croteau
+ * IGME-102: Assignment Name, m/d/24
+ * Description and interaction instructions
+ */
+
+"use strict"; //catch some common coding errors
+
+/* Global variables */
+
+/**
+ * setup :
+ */
+function setup() {
+   createCanvas(400, 400);
+   background("green");
+}
+
+/**
+ * draw :
+ */
+function draw() {
+
+}
